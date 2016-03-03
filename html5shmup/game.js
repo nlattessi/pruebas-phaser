b74a79a7c0a5d96972a7d3c6c51015bcabd92060
Nahuel Lattessi <n_lattessi@hotmail.com>
2016-03-03 19:10:59 -0300

    shmup afternoon2

--- html5shmup/game.js
+++ html5shmup/game.js
@@ -10,11 +10,20 @@ BasicGame.Game.prototype = {
     this.load.image('bullet', 'assets/bullet.png');
     this.load.spritesheet('greenEnemy', 'assets/enemy.png', 32, 32);
     this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
+    this.load.spritesheet('player', 'assets/player.png', 64, 64);
   },
 
   create: function () {
 
     this.sea = this.add.tileSprite(0, 0, 800, 600, 'sea');
+
+    this.player = this.add.sprite(400, 550, 'player');
+    this.player.anchor.setTo(0.5, 0.5);
+    this.player.animations.add('fly', [ 0, 1, 2 ], 20, true);
+    this.player.play('fly');
+    this.physics.enable(this.player, Phaser.Physics.ARCADE);
+    this.player.speed = 300;
+    this.player.body.collideWorldBounds = true;
 
     this.enemy = this.add.sprite(400, 200, 'greenEnemy');
     this.enemy.animations.add('fly', [ 0, 1, 2 ], 20, true);
@@ -22,19 +31,52 @@ BasicGame.Game.prototype = {
     this.enemy.anchor.setTo(0.5, 0.5);
     this.physics.enable(this.enemy, Phaser.Physics.ARCADE);
 
-    this.bullet = this.add.sprite(400, 300, 'bullet');
-    this.bullet.anchor.setTo(0.5, 0.5);
-    this.physics.enable(this.bullet, Phaser.Physics.ARCADE);
-    this.bullet.body.velocity.y = -500;
+    // this.bullet = this.add.sprite(400, 300, 'bullet');
+    // this.bullet.anchor.setTo(0.5, 0.5);
+    // this.physics.enable(this.bullet, Phaser.Physics.ARCADE);
+    // this.bullet.body.velocity.y = -500;
+    this.bullets = [];
+
+    this.cursors = this.input.keyboard.createCursorKeys();
 
   },
 
   update: function () {
     //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
     this.sea.tilePosition.y += 0.2;
-    this.physics.arcade.overlap(
-      this.bullet, this.enemy, this.enemyHit, null, this
-    );
+    // this.physics.arcade.overlap(
+    //   this.bullet, this.enemy, this.enemyHit, null, this
+    // );
+    // for (var i = 0; i < this.bullets.length; i++) {
+    //   this.physics.arcade.overlap(
+    //     this.bullets[i], this.enemy, this.enemyHit, null, this
+    //   );
+    // }
+
+    this.player.body.velocity.x = 0;
+    this.player.body.velocity.y = 0;
+
+    if (this.cursors.left.isDown) {
+      this.player.body.velocity.x = -this.player.speed;
+    } else if (this.cursors.right.isDown) {
+      this.player.body.velocity.x = this.player.speed;
+    }
+
+    if (this.cursors.up.isDown) {
+      this.player.body.velocity.y = -this.player.speed;
+    } else if (this.cursors.down.isDown) {
+      this.player.body.velocity.y = this.player.speed;
+    }
+
+    if (this.input.activePointer.isDown &&
+        this.physics.arcade.distanceToPointer(this.player) > 15) {
+      this.physics.arcade.moveToPointer(this.player, this.player.speed);
+    }
+
+    if (this.input.keyboard.isDown(Phaser.Keyboard.Z) ||
+        this.input.activePointer.isDown) {
+      this.fire();
+    }
   },
 
   render: function () {
@@ -59,6 +101,14 @@ BasicGame.Game.prototype = {
     explosion.anchor.setTo(0.5, 0.5);
     explosion.animations.add('boom');
     explosion.play('boom', 15, false, true);
+  },
+
+  fire: function() {
+    var bullet = this.add.sprite(this.player.x, this.player.y - 20, 'bullet');
+    bullet.anchor.setTo(0.5, 0.5);
+    this.physics.enable(bullet, Phaser.Physics.ARCADE);
+    bullet.body.velocity.y = -500;
+    this.bullets.push(bullet);
   },
 
 };
